649bb583c4fd53c682cb45ad39396a000b0676ea
Mohammad Javad <160188386+JajaBro01@users.noreply.github.com>
2024-02-17 00:47:10 +0330

    Update ChatGPT.py
    change python file

--- ChatGPT.py
+++ ChatGPT.py
@@ -37,23 +37,3 @@ async def client(client, message):
 
 app.run()
 
-
-
-# ChatGPT-Telegram
-ChatGPT client for telegram with python
-
-## Installing libraries
-```bash
-pip install pyrogram
-```
-```bash
-pip install requests
-```
-
-## Connection
-Put your account's api_id and api_hash in lines 6 and 7
-
-## run project
-```bash
-python3 chatGPT.py
-```
